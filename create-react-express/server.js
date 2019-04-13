1d439c6094583bfab19a63c13dbb9fa3047fb45c
ianschu <ianschu7@gmail.com>
2019-04-12 20:08:51 -0400

    removed unneeded files

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Require all models
-var db = require("./client/models");
+// var db = require("./client/models");
 
 mongoose.connect("mongodb://127.0.0.1:27017/Final", { useNewUrlParser: true })
 
